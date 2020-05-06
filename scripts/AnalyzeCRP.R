2f7bc4c2f05f86d936d13fe8064cd3b5909d3c74
noegit19 <56736946+noegit19@users.noreply.github.com>
2020-05-05 20:42:41 -0400

    Update AnalyzeCRP.R

--- scripts/AnalyzeCRP.R
+++ scripts/AnalyzeCRP.R
@@ -38,6 +38,22 @@ sink('data_output/CRP_regression.txt', append = TRUE)
 print(CRP.regression)
 sink()
 
+## Access only the columns with RNA Expression
+names(IBS)[37:286]
+
+##  Make a data frame of list type
+storage <- list()
+
+## Linear regression for each expressed gene
+for(i in names(IBS)[37:286]){
+  storage[[i]]  <- lm(get(i) ~ C.Reactive.Protein..CRP...mg.mL., IBS)
+}
+
+## Output the regresion report to a fie
+sink('../data_output/GenesvsCRP.txt', append = TRUE)
+print(storage)
+sink()
+
 ## ANOVA: IBS-subtype vs. Bloodwork parameter
 ## http://www.sthda.com/english/wiki/one-way-anova-test-in-r
 CRP.aov <- aov(CRP ~ IBS.subtype, data = IBS)
